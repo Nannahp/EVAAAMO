2b21a96622102a09ca8511716ed804bf08513a27
Nannahp <nannahofgaard@hotmail.com>
2024-12-03 15:23:33 +0100

    video and pictures are now Media

--- src/main/java/com/example/evaaamo/controllers/Hello.java
+++ src/main/java/com/example/evaaamo/controllers/Hello.java
@@ -1,21 +1,19 @@
 package com.example.evaaamo.controllers;
 
-import com.example.evaaamo.entities.Picture;
-import com.example.evaaamo.entities.Video;
-import com.example.evaaamo.repositories.PictureRepository;
-import com.example.evaaamo.repositories.VideoRepository;
+import com.example.evaaamo.entities.Media;
+import com.example.evaaamo.repositories.MediaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+
 @Controller
 public class Hello {
     @Autowired
-    PictureRepository pictureRepository;
+    MediaRepository mediaRepository;
 
-    @Autowired
-    VideoRepository videoRepository;
     @GetMapping("/")
     public String index() {
         return "index";
@@ -28,10 +26,10 @@ public class Hello {
 
     @GetMapping("/testing")
     public String testing(Model model) {
-        Picture picture = pictureRepository.findById(1).orElseThrow(() -> new IllegalArgumentException("Picture not found"));
-        Video video = videoRepository.findById(1).orElseThrow(() -> new IllegalArgumentException("Video not found"));
-        model.addAttribute("picture", picture);
-        model.addAttribute("video", video);
+        Media pic = mediaRepository.findById(1).orElseThrow(() -> new IllegalArgumentException("Picture not found"));
+        Media vid = mediaRepository.findById(2).orElseThrow(() -> new IllegalArgumentException("Video not found"));
+        model.addAttribute("picture", pic);
+        model.addAttribute("video", vid);
         return "testing";
     }
 }
--- src/main/java/com/example/evaaamo/repositories/PictureRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.evaaamo.repositories;
-
-import com.example.evaaamo.entities.Picture;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PictureRepository extends JpaRepository<Picture, Integer> {
-}
